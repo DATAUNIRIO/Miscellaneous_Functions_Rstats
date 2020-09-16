012d7e48eff69ed45f5c28fc6dcbae7b8a8aaa6f
DATAUNIRIO <duttross@gmail.com>
2020-09-16 11:47:18 -0300

    back

--- criar_nomes.R
+++ criar_nomes.R
@@ -13,6 +13,13 @@ criar_nomes <-function(string){
   gsub("\\s+", "_", tolower(string))
 }
 
+exemplo<-paste0("EXEMPLO DE FUNCIONAMENTO \n
+nomes<-names(df)  \n
+source('https://raw.githubusercontent.com/DATAUNIRIO/Miscellaneous_Functions_Rstats/master/criar_nomes.R',encoding = 'UTF-8') \n
+novos_nomes<-criar_nomes(nomes)  \n
+colnames(df)<-novos_nomes")
+message(exemplo)
+
 # Teste de funcionamento
 #string <- c("R package","meu teste de funcionamento","Estatística e Probabilidade","Estatística para a Educação Básica")
 #criar_nomes(string)
